3fbf9d2aacb0da94dc8f6c3d349ce7752ead813b
Nikola Vujovic <nikolavujovickv@gmail.com>
2026-06-14 17:31:07 +0200

    Kreirana Stats sekcija

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import HeroSection from './sections/HeroSection'
 import Navbar from './components/NavBar'
+import StatsSection from './sections/StatsSection'
 
 function App() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <StatsSection></StatsSection>
     </>
   )
 }
---
+++ frontend/src/sections/StatsSection.tsx
@@ -0,0 +1,26 @@
+import '../styles/stats.css'
+
+const StatsSection = () =>{ 
+  return(
+    <div className="stats_section">
+      <div className="components">
+        <h2>4+</h2>
+        <p>Years Coding</p>
+      </div>
+      <div className="components">
+        <h2>5+</h2>
+        <p>projects</p>
+      </div>
+      <div className="components">
+        <h2>15+</h2>
+        <p>Technologies</p>
+      </div>
+      <div className="components">
+        <h2>100%</h2>
+        <p>passion</p>
+      </div>
+    </div>
+  )
+}
+
+export default StatsSection
